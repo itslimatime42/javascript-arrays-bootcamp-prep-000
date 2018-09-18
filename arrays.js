c9abe0724abf7229d7faae32773036d2b3457333
Robert Schulte <schulte.robertw@outlook.com>
2018-09-18 04:38:52 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -36,4 +36,9 @@ function removeElementFromBeginningOfArray(array) {
 
 function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array) {
+  
 }
