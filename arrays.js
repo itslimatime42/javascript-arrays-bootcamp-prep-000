cf9ff10ec8af61f1a5b9af8665fe58aadcc5f81d
Robert Schulte <schulte.robertw@outlook.com>
2018-09-18 04:28:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,7 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 function addElementToBeginningOfArray(array, element) {
   array.unshift(element)
 }
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  
+}
