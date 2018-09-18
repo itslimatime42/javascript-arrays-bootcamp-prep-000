ac9bcb86fd3e1c6aefd989decd555f62c3acdf1c
Robert Schulte <schulte.robertw@outlook.com>
2018-09-18 04:39:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -40,5 +40,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  
+  var newArray = array.slice(0, array.length)
+  return newArray
 }
